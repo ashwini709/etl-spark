8c701122cb9b83295975dfe35e0a579491382f1a
ashwini <ashwini@kkvesper.jp>
2018-12-13 22:07:26 +0900

    Setup to connect to redshift

--- build.sbt
+++ build.sbt
@@ -1,6 +1,9 @@
 name := "etl-spark"
 version := "1.0"
 
-scalaVersion := "2.12.7"
+scalaVersion := "2.11.12"
 
 libraryDependencies += "org.apache.spark" %% "spark-core" % "2.4.0"
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.4.0"
+libraryDependencies += "com.databricks" %% "spark-redshift" % "3.0.0-preview1"
+libraryDependencies += "com.amazonaws" % "aws-java-sdk-redshift" % "1.11.285"
--- src/main/scala/nexusetl/Verifier.scala
+++ src/main/scala/nexusetl/Verifier.scala
@@ -1,8 +1,8 @@
 package nexusetl
 
-import org.apache.spark.SparkConf
-import org.apache.spark.SparkContext
-import org.apache.spark.rdd.RDD
+import com.amazonaws.auth._
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.{SparkConf, SparkContext}
 
 object Verifier {
   val conf: SparkConf = new SparkConf().setAppName("Verifier").setMaster("local[2]")
@@ -19,6 +19,34 @@ object Verifier {
     }.cache()
 
     shopReservationRDD.countByKey()
+
+    val provider: AWSCredentialsProvider = new AWSStaticCredentialsProvider(
+      new BasicAWSCredentials("0LYDLQ3LM2M9SC62IU70", "IfR/qA2lsrKFrcOh/eR2JUr2PuJnVC7EnbQpqiH7")
+    )
+    val credentials: AWSSessionCredentials = provider.getCredentials.asInstanceOf[AWSSessionCredentials]
+    val token = credentials.getSessionToken
+    val awsAccessKey = credentials.getAWSAccessKeyId
+    val awsSecretKey = credentials.getAWSSecretKey
+
+
+    val spark = new SparkSession.Builder().getOrCreate()
+    val salesDF = spark.read
+      .format("com.databricks.spark.redshift")
+      .option("url", jdbcURL) //Provide the JDBC URL
+      .option("jdbcdriver", "com.amazon.redshift.jdbc42.Driver")
+      .option("tempdir", "s3://spark-test")
+      .option("temporary_aws_access_key_id", awsAccessKey)
+      .option("temporary_aws_secret_access_key", awsSecretKey)
+      .option("temporary_aws_session_token", token)
+      .option("query", "select shop_id, reservation_id from reservations_scala")
+      .load()
+
+    salesDF.createOrReplaceTempView("sales_from_redshift")
+
+    val newSalesDF = spark.sql("SELECT count(*) FROM sales_from_redshift")
+
+    println(newSalesDF)
+
 
     sc.stop()
   }
