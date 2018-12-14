414d483e82aa88e8cb5d765ebb6d4816c849d3e7
ashwini <ashwini@kkvesper.jp>
2018-12-14 15:31:20 +0900

    Load data from textFile as a DataFrame

--- src/main/scala/nexusetl/Verifier.scala
+++ src/main/scala/nexusetl/Verifier.scala
@@ -1,9 +1,12 @@
 package nexusetl
 
+import com.amazonaws.auth.profile.ProfileCredentialsProvider
+import com.amazonaws.auth.{AWSCredentialsProviderChain, EnvironmentVariableCredentialsProvider}
 import com.typesafe.config.ConfigFactory
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.{SparkConf, SparkContext}
 
+case class Reservation(shop: String, id: String)
 object Verifier {
   private val config = ConfigFactory.load()
   private val url = config.getString("redshift.url")
@@ -12,35 +15,46 @@ object Verifier {
   val conf: SparkConf = new SparkConf().setAppName("Verifier").setMaster("local[2]")
   val sc: SparkContext = new SparkContext(conf)
 
-
   def main(args: Array[String]) {
-//    val fileName: String = if (args.nonEmpty) args(0) else "*"
-//    val rdd = sc.textFile(fileName)
-//
-//    val shopReservationRDD: RDD[(String, String)] = rdd.map { s =>
-//      val a = s.split(",")
-//      val f = a(0).split(" ")
-//      (f(4), f(2))
-//    }.cache()
-//
-//    shopReservationRDD.countByKey()
-
-
     val spark = new SparkSession.Builder().getOrCreate()
-    val salesDF = spark.read
+
+    import spark.implicits._
+    val shopReservationDF = spark.sparkContext.textFile("create_f.txt").map { s =>
+      val a = s.split(",")
+      val f = a(0).split(" ")
+      (f(4), f(2))
+    }.map(attr ⇒ Reservation(attr._1, attr._2)).toDF()
+
+    println(shopReservationDF.printSchema())
+    shopReservationDF.createOrReplaceTempView("shop_reservations")
+
+    val awsCredentials = new AWSCredentialsProviderChain(new
+      EnvironmentVariableCredentialsProvider(), new
+      ProfileCredentialsProvider())
+
+    sc.hadoopConfiguration.set("fs.s3a.access.key", awsCredentials.getCredentials.getAWSAccessKeyId)
+    sc.hadoopConfiguration.set("fs.s3a.secret.key", awsCredentials.getCredentials.getAWSSecretKey)
+
+    val sales_from_redshift = spark.read
       .format("com.databricks.spark.redshift")
-      .option("url", url) //Provide the JDBC URL
-      .option("tempdir", "s3://spark-test/tmp")
+      .option("url", url)
+      .option("tempdir", "s3a://kkvesper-nexus-staging/verifier/dev/")
       .option("aws-iam-role", iam_role)
       .option("forward_spark_s3_credentials", true)
       .option("query", "select shop_id, reservation_id from reservations_scala")
       .load()
 
-    salesDF.createOrReplaceTempView("sales_from_redshift")
+    sales_from_redshift.createOrReplaceTempView("tmp_reservations")
 
-    val newSalesDF = spark.sql("SELECT count(*) FROM sales_from_redshift")
+    val salesFromRedshiftDF = sales_from_redshift.select("shop_id", "reservation_id")
 
-    println(newSalesDF)
+    val joinedDF = shopReservationDF
+      .join(salesFromRedshiftDF, shopReservationDF("shop") === salesFromRedshiftDF("shop_id"))
+      .join(salesFromRedshiftDF, shopReservationDF("id") === salesFromRedshiftDF("reservation_id"))
+
+    println("=" * 100)
+    println(joinedDF)
+    println("=" * 100)
 
     sc.stop()
   }
